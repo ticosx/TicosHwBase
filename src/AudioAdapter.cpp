e97059ec5fcefe9d36bad3b7eb00eee6b245a9e6
Sawyer <sawyer@SawyerdeMacBook-Pro.local>
2022-05-11 00:13:43 +0800

    refactor: 支持更多codec配置操作

--- src/AudioAdapter.cpp
+++ src/AudioAdapter.cpp
@@ -7,28 +7,3 @@
 AudioAdapter::AudioAdapter(Adafruit_I2CDevice *i2cdevice) {
   this->i2cdevice = i2cdevice;
 }
-
-/*!
- *    @brief  初始化音频设备
- */
-bool AudioAdapter::init() {
-  return initDev();
-}
-
-/*!
- *    @brief  设置音量
- *    @param  volume 要设置的音量。音量最大为 100， 最小为 0 （静音）
- *    @return 成功设置音量则返回 true
- */
-bool AudioAdapter::setVolume(int8_t volume) {
-  return writeVolume2Dev(volume);
-}
-
-/*!
- *    @brief  获得当前音量
- *    @return 当前音量
- */
-int8_t AudioAdapter::getVolume() {
-  //TODO: Read volume from device
-  return 0;
-}
--- src/AudioAdapter.h
+++ src/AudioAdapter.h
@@ -5,6 +5,7 @@
 
 #include <Adafruit_I2CDevice.h>
 
+#include "AudioHal.h"
 /*!
  * @brief The class which defines a device supports audio playing/recording
  */
@@ -12,19 +13,51 @@ class AudioAdapter {
 public:
   AudioAdapter(Adafruit_I2CDevice *i2cdevice);
 
-  bool setVolume(int8_t volume);
-  int8_t getVolume();
-  bool init();
-
-protected:
-
+/*!
+ *    @brief  设置音量
+ *    @param  volume 要设置的音量。音量最大为 100， 最小为 0 （静音）
+ *    @return 成功设置音量则返回 true
+ */
+  virtual bool setVolume(int8_t volume) = 0;
+/*!
+ *    @brief  获得当前音量
+ *    @return 当前音量
+ */
+  virtual int8_t getVolume() = 0;
 /*!
  *    @brief  初始化设备
  *    @return 初始化成功则返回 true
  */
-  virtual bool initDev() = 0;
-  virtual bool writeVolume2Dev(int8_t volume) = 0;
+  virtual bool init() = 0;
+  
+/*!
+ *    @brief  设置采样位宽
+ *    @param  bits 采样位宽。常见值为 8, 16, 24, 32 等
+ *    @return 设置成功则返回 true
+ */
+  virtual bool setBitsPerSample(int bits) = 0;
+/*!
+ *    @brief  设置声道数
+ *    @param  chan 声道数。可选值 1, 2
+ *    @return 设置成功则返回 true
+ */
+  virtual bool setChannels(int chan) = 0;
 
+/*!
+ *    @brief  启动音频外设
+ *    @param  mode 外设工作模式，放音、录音还是全双工
+ *    @return 设置成功则返回 true
+ */
+  virtual bool start(audio_hal_codec_mode_t mode) = 0;
+
+/*!
+ *    @brief  停止音频外设
+ *    @param  mode 外设工作模式，放音、录音还是全双工
+ *    @return 设置成功则返回 true
+ */
+  virtual bool stop(audio_hal_codec_mode_t mode) = 0;
+
+protected:
   Adafruit_I2CDevice *i2cdevice;
 };
 #endif // Audio_Adapter_h
