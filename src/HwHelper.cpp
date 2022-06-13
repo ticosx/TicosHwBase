695a0b6d72d30fca1a4bf9b99343ad1cdf513a2d
Sawyer <sawyer@SawyerdeMacBook-Pro.local>
2022-06-13 18:09:19 +0800

    refactor: 增加 I2C 调用接口

--- src/HwHelper.cpp
+++ src/HwHelper.cpp
@@ -3,25 +3,28 @@
 /**
  * @brief 读取指定寄存器的值
  *
- * @param addr: 寄存器地址
+ * @param i2cdevice: 要访问的 I2C 设备实例，类型为 Adafruit_I2CDevice
+ * @param addr: 要读取的寄存器地址
+ * @param data: 用于保存读取数据的地址
+ * @param len: 要读取的数据长度
  * 
- * @return  指定寄存器的值
+ * @return True 读取成功返回 true, 否则返回 false.
  *
  */
-uint8_t i2cReadReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr){
-    uint8_t rdData; 
-    i2cdevice->write_then_read(&addr, sizeof(addr), &rdData, 1, true);
-    return rdData;
-
+bool i2cReadReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t *data, uint8_t len){
+    return i2cdevice->write_then_read(&addr, sizeof(addr), data, len, true);
 }
 
 /**
  * @brief 设置指定寄存器的值
  *
+ * @param i2cdevice: 要访问的 I2C 设备实例，类型为 Adafruit_I2CDevice
  * @param addr: 寄存器地址
  * @param data: 要设置的值
+ * 
+ * @return True 设置成功返回 true, 否则返回 false.
  *
  */
-void i2cWriteReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t data) {
-    i2cdevice->write( &data, sizeof(data), true, &addr, sizeof(addr));
+bool i2cWriteReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t data) {
+    return i2cdevice->write( &data, sizeof(data), true, &addr, sizeof(addr));
 }
--- src/HwHelper.h
+++ src/HwHelper.h
@@ -4,10 +4,11 @@
 #include <Arduino.h>
 #include <Adafruit_I2CDevice.h>
 
-#define I2C_READ_REG(addr)  i2cReadReg(i2cdevice, addr)
+#define I2C_READ_REG(addr, data)  i2cReadReg(i2cdevice, addr, &data, sizeof(data))
+#define I2C_READ_ARRAY(addr, buf)  i2cReadReg(i2cdevice, addr, buf, sizeof(buf))
 #define I2C_WRITE_REG(addr, data)  i2cWriteReg(i2cdevice, addr, data)
     
-uint8_t i2cReadReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr);
-void i2cWriteReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t data); 
+bool i2cReadReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t *data, uint8_t len);
+bool i2cWriteReg(Adafruit_I2CDevice *i2cdevice, uint8_t addr, uint8_t data); 
 
 #endif
