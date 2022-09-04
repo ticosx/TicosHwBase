1cc9bdf933fe220c316477bd29571fb4307861e6
Sawyer <35255629+shaoyie@users.noreply.github.com>
2022-09-04 21:53:25 +0800

    refactor: 支持 LCD 旋转配置

--- src/DisplayAdapter.cpp
+++ src/DisplayAdapter.cpp
@@ -3,7 +3,7 @@
 /*!
  *    @brief  创建一个 DisaplyAdapter
  */
-DisplayAdapter::DisplayAdapter(int16_t screenWidth, int16_t screenHeight, int8_t bpp) : 
-  screenWidth(screenWidth), screenHeight(screenHeight) , bpp(bpp)
+DisplayAdapter::DisplayAdapter(int16_t screenWidth, int16_t screenHeight, int8_t bpp, int8_t rotation) : 
+  screenWidth(screenWidth), screenHeight(screenHeight) , bpp(bpp), rotation(rotation)
 {
 }
--- src/DisplayAdapter.h
+++ src/DisplayAdapter.h
@@ -8,11 +8,12 @@
  */
 class DisplayAdapter : public HardwareAdapter {
   public:
-    DisplayAdapter(int16_t screenWidth, int16_t screenHeight, int8_t bpp);
+    DisplayAdapter(int16_t screenWidth, int16_t screenHeight, int8_t bpp, int8_t rotation=1);
     virtual ~DisplayAdapter(){};
     virtual int16_t getScreenWidth() {return screenWidth;};
     virtual int16_t getScreenHeight() {return screenHeight;};
     virtual int16_t getColorBit() {return bpp;};
+    virtual int16_t getRotation() {return rotation;};
     virtual bool setLight(int8_t) {return true;};
     virtual bool trunOn() {return true;};
     virtual bool trunOff() {return true;};
@@ -21,5 +22,6 @@ class DisplayAdapter : public HardwareAdapter {
     int16_t screenHeight;
     // Bits per pixel
     int8_t bpp;
+    int8_t rotation;
 };
 #endif
